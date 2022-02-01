fcc1c3bd0c046f2e0676ac441bb34936674386ea
TylerOliveira <55139667+TylerOliveira@users.noreply.github.com>
2022-02-01 01:37:46 -0800

    seeded seeds.sql with development data

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,32 @@ USE employees_db;
 
 CREATE TABLE departments (
   id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
+  PRIMARY KEY (id),
   FOREIGN KEY (department_id) 
   REFERENCES departments(id)
+  ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
+  PRIMARY KEY (id),
   FOREIGN KEY (role_id)
-  REFERENCES role(id),
+  REFERENCES roles(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 );
